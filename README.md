ad94a393230aa58e63c6b7a1c8d9d4d2c3b07e59
Geeta Ahirwar <geeta.ahirwar589@gmail.com>
2024-06-20 14:03:11 -0400

    Second

--- README.md
+++ README.md
@@ -4,3 +4,9 @@ Project Submission date : 20 Jun 2024
 Description : For Recipe Project User need to Register first, and data will strore in database
               as MonogoDB database if not registered once registration done User can login for Search Recipe.
               After Login Successfully , he can Search all recipes and Favourite Recipes based area
+
+Technologies used : 
+            HTML , CSS , ReactJS , Nodejs , JavaScript etc.
+            MongoDB as Database.
+
+API Used : https://www.themealdb.com/api/json/v1/1/search.php?f=a
--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -1,31 +1,45 @@
-import RecipeSearch from "../RecipeSearch";
+import { useState } from "react";
+import { Link } from "react-router-dom";
+import axios from 'axios'
 import { useNavigate } from "react-router-dom";
 
 function Login() {
 
+  const [email, setEmail] = useState();
+  const [password, setPassword] = useState();
   const navigate = useNavigate();
 
-  const handleSubmit = e => {
+  const handleSubmit = (e) => {
     e.preventDefault();
-    navigate('/RecipeForm')
-
-  }
+    axios
+      .post("http://localhost:4000/user/UserLogin", { email, password })
+      .then((result) => console.log(result))
+      .catch((err) => console.log(err));
+    navigate("/RecipeSearch");
+  };
 
   return (
     <>
-      <div className="login"> 
+      <div className="login">
         <form onSubmit={handleSubmit}>
-        <label htmlFor="">Login </label> 
-          <input type="text" autoComplete="off" name="login" />
-           <label htmlFor="">Password</label> 
-          <input type="password" autoComplete="off" name="password" />
+          <label htmlFor="">Login </label>
+          <input
+            type="text"
+            autoComplete="off"
+            name="login"
+            onChange={(e) => setEmail(e.target.value)}
+          />
+          <label htmlFor="">Password</label>
+          <input
+            type="password"
+            autoComplete="off"
+            name="password"
+            onChange={(e) => setEmail(e.target.value)}
+          />
+        </form>
 
-          </form> 
-          
-          <input type="Submit" />
-          
+        <input type="Submit" />
       </div>
-     
     </>
   );
 }
